2f42d0046f23afb0672e6b6beab32e4d75808216
TelAndr <atelikh@gmail.com>
2025-03-26 22:23:29 +0300

    add space after { in file Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class Utils {
-    public static int getRandomInt(int minValue, int maxValue){
+    public static int getRandomInt(int minValue, int maxValue) {
         return minValue + (int) (Math.random() * (maxValue - minValue + 1));
     }
 }
