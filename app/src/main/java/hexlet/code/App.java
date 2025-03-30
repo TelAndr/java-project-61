4b55e4cbd1e5b1c4d494e9799594d03511ad7150
TelAndr <atelikh@gmail.com>
2025-03-30 21:52:40 +0300

    check linter style in file App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+
 import hexlet.code.games.*;
 
 import java.util.Scanner;
@@ -9,9 +10,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Let's try again, " + App.username + "!");
         System.out.println(
-                "Please enter the game number and press Enter.\n" +
-                        "1 - Greet\n" + "2 - Even\n" + "3 - Calc\n" + "4 - GCD\n" +
-                        "5 - Progression\n" + "6 - Prime\n" + "0 - Exit");
+                "Please enter the game number and press Enter.\n"
+                        + "1 - Greet\n" + "2 - Even\n" + "3 - Calc\n" + "4 - GCD\n"
+                        + "5 - Progression\n" + "6 - Prime\n" + "0 - Exit");
         String strNumGame = USER_INPUT.nextLine();
         int numGame = Integer.parseInt(strNumGame);
         System.out.println("Your choice:" + numGame + "\n");
@@ -20,29 +21,29 @@ public class App {
         //App.username = App.USER_INPUT.nextLine();
         //System.out.println("Hello, " + App.username + "!");
         Engine.printUserName();
-            switch (numGame) {
-                case 0:
-                    break;
-                case 1:
-                    //Cli.askUser();
-                    break;
-                case 2:
-                    Parity.run();
-                    break;
-                case 3:
-                    Calculate.run();
-                    break;
-                case 4:
-                    GCD.run();
-                    break;
-                case 5:
-                    Progression.run();
-                    break;
-                case 6:
-                    Prime.run();
-                    break;
-                default:
-                    System.out.println("Error input!");
-            }
+        switch (numGame) {
+            case 0:
+                break;
+            case 1:
+                //Cli.askUser();
+                break;
+            case 2:
+                Parity.run();
+                break;
+            case 3:
+                Calculate.run();
+                break;
+            case 4:
+                GCD.run();
+                break;
+            case 5:
+                Progression.run();
+                break;
+            case 6:
+                Prime.run();
+                break;
+            default:
+                System.out.println("Error input!");
+        }
     }
 }
