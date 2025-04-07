79d2ced07b5dbadb4b7e9de1b03529d14f08f7d3
TelAndr <atelikh@gmail.com>
2025-04-07 21:30:16 +0300

    move char + in new line

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -19,8 +19,8 @@ public class Engine {
             String strAnsUserOper = App.USER_INPUT.nextLine();
             System.out.println("Your answer: " + strAnsUserOper);
             if (!strCorrectAnswer[iAtt][1].equalsIgnoreCase(strAnsUserOper)) {
-                System.out.println("'" + strAnsUserOper + "' is wrong answer ;(. Correct answer was '" +
-                        strCorrectAnswer[iAtt][1] + "'.");
+                System.out.println("'" + strAnsUserOper + "' is wrong answer ;(. Correct answer was '"
+                        + strCorrectAnswer[iAtt][1] + "'.");
                 System.out.println("Let's try again, " + App.username + "!");
                 break;
             } else {
